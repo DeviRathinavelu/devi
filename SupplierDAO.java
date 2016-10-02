58889cdf267cac5e4d0bbc8d78907ea54c2972c4
Devi <devisree.cse@gmail.com>
2016-10-02 14:27:51 +0530

    Add files via upload

--- SupplierDAO.java
+++ SupplierDAO.java
@@ -1,16 +1,12 @@
 package com.devi.bags.dao;
 
-import java.util.List;
-
 import com.devi.bags.model.Supplier;
 
 public interface SupplierDAO {
 
-	public boolean create(Supplier supplier);	
+	public boolean save(Supplier supplier);	
     public boolean update(Supplier supplier);
-    public boolean delete(String id);
-    public SupplierDAO get(String id);
-    public List<SupplierDAO>list();
-
+    public void delete(String id);
+   
 }
 
--- SupplierDAOImpl.java
+++ SupplierDAOImpl.java
@@ -1,73 +1,50 @@
 package com.devi.bags.dao;
-import java.util.List;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import com.devi.bags.model.Product;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devi.bags.model.Supplier;
 
 
 @Repository
 public class SupplierDAOImpl implements SupplierDAO {
+	@SuppressWarnings("unused")
 	@Autowired
 	private Supplier supplier;
 	@Autowired
 	private SessionFactory sessionFactory;
+	public SupplierDAOImpl(SessionFactory sessionfactory)
+	{
+		this.sessionFactory=sessionfactory;
+	}
 
+	@Transactional
 	public boolean save(Supplier supplier) {
 		try {
 			sessionFactory.getCurrentSession().save(supplier);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		}
-
+		}// TODO Auto-generated method stub
 		return true;
 	}
-
+	@Transactional
 	public boolean update(Supplier supplier) {
 		try {
 			sessionFactory.getCurrentSession().update(supplier);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
-		}
-
+	}
+		// TODO Auto-generated method stub
 		return true;
 	}
-
-	public boolean delete(String id) {
-		try {
-			sessionFactory.getCurrentSession().delete(supplier);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
+	@Transactional
+	public void delete(String id) {
+		Supplier SupplierToDelete = new Supplier();
+		SupplierToDelete.setId(id);
+			sessionFactory.getCurrentSession().delete(SupplierToDelete);
 	}
 
-	public Supplier get(String id) {
-		String hql = "from Supplier where id='" + id + "'";
-		Query query = sessionFactory.getCurrentSession().createQuery(hql);
-		List<Supplier> list =(List<Supplier>)query;
-		if (list != null || list.isEmpty()) {
-			return null;
-		}
-		return list.get(0);
-	}
-
-	public List<Supplier> list() {
-		return null;
-	}
-
-	public boolean create(Supplier supplier) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public SupplierDAO get(String id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
